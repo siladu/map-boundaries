7088fad93552b6faa0584f753c1ec3b459333cb7
simon.dudley <simon.dudley@autotrader.co.uk>
2018-04-17 22:15:02 +0100

    Add google maps demo code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
+import { KEY } from "./secrets/google.key.js";
+
+const MyMapComponent = withScriptjs(
+  withGoogleMap(props => (
+    <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+      {props.isMarkerShown && (
+        <Marker position={{ lat: -34.397, lng: 150.644 }} />
+      )}
+    </GoogleMap>
+  ))
+);
 
 class App extends Component {
   render() {
-    const test = { test1: "test2" };
-
     return (
       <div className="App">
         <header className="App-header">
@@ -15,6 +30,13 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <MyMapComponent
+          isMarkerShown
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${KEY}&v=3.exp&libraries=geometry,drawing,places`}
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
       </div>
     );
   }
