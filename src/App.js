167751751e8d4b0760d5ec22cb6aaf6c1430b975
simon.dudley <simon.dudley@autotrader.co.uk>
2018-04-17 21:24:44 +0100

    Made lint-staged prettier command match vscode plugin default (stopped using singlequotes)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
-    const test = { test1: 'test2' };
+    const test = { test1: "test2" };
 
     return (
       <div className="App">
